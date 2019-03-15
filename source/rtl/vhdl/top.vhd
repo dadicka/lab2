af7fdb3a0de7082f18fa563317be509f1e98db08
Jovana <katarinadadic9@gmail.com>
2019-03-15 10:32:58 +0100

    dadicka

--- source/rtl/vhdl/top.vhd
+++ source/rtl/vhdl/top.vhd
@@ -24,6 +24,8 @@ entity top is
     clk_i          : in  std_logic;
     reset_n_i      : in  std_logic;
     -- vga
+        direct_mode_i     : in std_logic;
+        display_mode_i : in std_logic_vector( 1 downto 0);
     vga_hsync_o    : out std_logic;
     vga_vsync_o    : out std_logic;
     blank_o        : out std_logic;
@@ -41,18 +43,18 @@ architecture rtl of top is
   constant RES_NUM : natural := 6;
 
   type t_param_array is array (0 to RES_NUM-1) of natural;
-  
+ 
   constant H_RES_ARRAY           : t_param_array := ( 0 => 64, 1 => 640,  2 => 800,  3 => 1024,  4 => 1152,  5 => 1280,  others => 0 );
   constant V_RES_ARRAY           : t_param_array := ( 0 => 48, 1 => 480,  2 => 600,  3 => 768,   4 => 864,   5 => 1024,  others => 0 );
   constant MEM_ADDR_WIDTH_ARRAY  : t_param_array := ( 0 => 12, 1 => 14,   2 => 13,   3 => 14,    4 => 14,    5 => 15,    others => 0 );
-  constant MEM_SIZE_ARRAY        : t_param_array := ( 0 => 48, 1 => 4800, 2 => 7500, 3 => 12576, 4 => 15552, 5 => 20480, others => 0 ); 
-  
+  constant MEM_SIZE_ARRAY        : t_param_array := ( 0 => 48, 1 => 4800, 2 => 7500, 3 => 12576, 4 => 15552, 5 => 20480, others => 0 );
+ 
   constant H_RES          : natural := H_RES_ARRAY(RES_TYPE);
   constant V_RES          : natural := V_RES_ARRAY(RES_TYPE);
   constant MEM_ADDR_WIDTH : natural := MEM_ADDR_WIDTH_ARRAY(RES_TYPE);
   constant MEM_SIZE       : natural := MEM_SIZE_ARRAY(RES_TYPE);
 
-  component vga_top is 
+  component vga_top is
     generic (
       H_RES                : natural := 640;
       V_RES                : natural := 480;
@@ -102,7 +104,7 @@ architecture rtl of top is
       blue_o              : out std_logic_vector(7 downto 0)
     );
   end component;
-  
+ 
   component ODDR2
   generic(
    DDR_ALIGNMENT : string := "NONE";
@@ -120,16 +122,16 @@ architecture rtl of top is
     S           : in  std_ulogic := 'L'
   );
   end component;
-  
-  
+ 
+ 
   constant update_period     : std_logic_vector(31 downto 0) := conv_std_logic_vector(1, 32);
-  
+ 
   constant GRAPH_MEM_ADDR_WIDTH : natural := MEM_ADDR_WIDTH + 6;-- graphics addres is scales with minumum char size 8*8 log2(64) = 6
-  
+ 
   -- text
   signal message_lenght      : std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);
   signal graphics_lenght     : std_logic_vector(GRAPH_MEM_ADDR_WIDTH-1 downto 0);
-  
+ 
   signal direct_mode         : std_logic;
   --
   signal font_size           : std_logic_vector(3 downto 0);
@@ -141,24 +143,31 @@ architecture rtl of top is
 
   signal char_we             : std_logic;
   signal char_address        : std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);
+  signal offset              : std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);
+  signal offset_next           : std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);
   signal char_value          : std_logic_vector(5 downto 0);
 
   signal pixel_address       : std_logic_vector(GRAPH_MEM_ADDR_WIDTH-1 downto 0);
   signal pixel_value         : std_logic_vector(GRAPH_MEM_DATA_WIDTH-1 downto 0);
   signal pixel_we            : std_logic;
+  signal add                      : std_logic_vector(9 downto 0);
+ 
+    signal counter                : std_logic_vector(10 downto 0);
+  signal counter_s                : std_logic_vector(10 downto 0);
+
 
   signal pix_clock_s         : std_logic;
   signal vga_rst_n_s         : std_logic;
   signal pix_clock_n         : std_logic;
-   
+  
   signal dir_red             : std_logic_vector(7 downto 0);
   signal dir_green           : std_logic_vector(7 downto 0);
   signal dir_blue            : std_logic_vector(7 downto 0);
   signal dir_pixel_column    : std_logic_vector(10 downto 0);
   signal dir_pixel_row       : std_logic_vector(10 downto 0);
-  
-   signal offset_text		 	  : std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);
-	signal counter_text : std_logic_vector(31 downto 0);
+ 
+  signal sec_cnt             : std_logic_vector(24 downto 0);
+  signal sec_cnt_next           : std_logic_vector(24 downto 0);
 
 begin
 
@@ -167,13 +176,13 @@ begin
                     conv_std_logic_vector(MEM_SIZE/16, MEM_ADDR_WIDTH)when (font_size = 2) else
                     conv_std_logic_vector(MEM_SIZE/4 , MEM_ADDR_WIDTH)when (font_size = 1) else
                     conv_std_logic_vector(MEM_SIZE   , MEM_ADDR_WIDTH);
-  
+ 
   graphics_lenght <= conv_std_logic_vector(MEM_SIZE*8*8, GRAPH_MEM_ADDR_WIDTH);
-  
+ 
   -- removed to inputs pin
   direct_mode <= '0';
-  display_mode     <="01";  -- 01 - text mode, 10 - graphics mode, 11 - text & graphics
-  
+  display_mode     <= "01";  -- 01 - text mode, 10 - graphics mode, 11 - text & graphics
+ 
   font_size        <= x"1";
   show_frame       <= '1';
   foreground_color <= x"FFFFFF";
@@ -182,7 +191,7 @@ begin
 
   clk5m_inst : ODDR2
   generic map(
-    DDR_ALIGNMENT => "NONE",  -- Sets output alignment to "NONE","C0", "C1" 
+    DDR_ALIGNMENT => "NONE",  -- Sets output alignment to "NONE","C0", "C1"
     INIT => '0',              -- Sets initial state of the Q output to '0' or '1'
     SRTYPE => "SYNC"          -- Specifies "SYNC" or "ASYNC" set/reset
   )
@@ -214,14 +223,14 @@ begin
     clk_i              => clk_i,
     reset_n_i          => reset_n_i,
     --
-    direct_mode_i      => direct_mode,
+    direct_mode_i      => direct_mode_i,
     dir_red_i          => dir_red,
     dir_green_i        => dir_green,
     dir_blue_i         => dir_blue,
     dir_pixel_column_o => dir_pixel_column,
     dir_pixel_row_o    => dir_pixel_row,
     -- cfg
-    display_mode_i     => display_mode,  -- 01 - text mode, 10 - graphics mode, 11 - text & graphics
+    display_mode_i     => display_mode_i,  -- 01 - text mode, 10 - graphics mode, 11 - text & graphics
     -- text mode interface
     text_addr_i        => char_address,
     text_data_i        => char_value,
@@ -246,59 +255,126 @@ begin
     sync_o             => sync_o,
     red_o              => red_o,
     green_o            => green_o,
-    blue_o             => blue_o     
+    blue_o             => blue_o    
   );
-  
+ 
   -- na osnovu signala iz vga_top modula dir_pixel_column i dir_pixel_row realizovati logiku koja genereise
   --dir_red
   --dir_green
   --dir_blue
  
- -- dir_red <= x"ff" when dir_pixel_column < 80 else-
-			--	x"ff" when dir_pixel_column < 160 else
-			--	x"00" when dir_pixel_column < 240 else
-			--	x"00" when dir_pixel_column < 320 else
-			--	x"ff" when dir_pixel_column < 400 else
-			--	x"ff" when dir_pixel_column < 480 else
-			--	x"00" when dir_pixel_column < 560 else
-			--	x"00";
---
---	dir_green <= x"ff" when dir_pixel_column < 80 else
-	--				x"ff" when dir_pixel_column < 160 else
-	--				x"ff" when dir_pixel_column < 240 else
-	--				x"ff" when dir_pixel_column < 320 else
-	--				x"00" when dir_pixel_column < 400 else
-	--				x"00" when dir_pixel_column < 480 else
-	--				x"00" when dir_pixel_column < 560 else
---					x"00";
-	--			 
-	--dir_blue <= x"ff" when dir_pixel_column < 80 else
---				  x"00" when dir_pixel_column < 160 else
---				  x"ff" when dir_pixel_column < 240 else
---				  x"00" when dir_pixel_column < 320 else
---				  x"ff" when dir_pixel_column < 400 else-
-	--			  x"00" when dir_pixel_column < 480 else				  
---				  x"ff" when dir_pixel_column < 560 else
-	--			  x"00";
+  dir_red <= x"ff" when dir_pixel_column < 80 else
+                 x"ff" when dir_pixel_column < 160 else
+                 x"00" when dir_pixel_column < 240 else
+                 x"00" when dir_pixel_column < 320 else
+                 x"ff" when dir_pixel_column < 400 else
+                 x"ff" when dir_pixel_column < 480 else
+                 x"00" when dir_pixel_column < 560 else
+                 x"00";
+
+    dir_green <= x"ff" when dir_pixel_column < 80 else
+                    x"ff" when dir_pixel_column < 160 else
+                    x"ff" when dir_pixel_column < 240 else
+                    x"ff" when dir_pixel_column < 320 else
+                    x"00" when dir_pixel_column < 400 else
+                    x"00" when dir_pixel_column < 480 else
+                    x"00" when dir_pixel_column < 560 else
+                    x"00";
+                 
+    dir_blue <= x"ff" when dir_pixel_column < 80 else
+                  x"00" when dir_pixel_column < 160 else
+                  x"ff" when dir_pixel_column < 240 else
+                  x"00" when dir_pixel_column < 320 else
+                  x"ff" when dir_pixel_column < 400 else
+                  x"00" when dir_pixel_column < 480 else
+                  x"ff" when dir_pixel_column < 560 else
+                  x"00";
+ 
   -- koristeci signale realizovati logiku koja pise po TXT_MEM
   --char_address
   --char_value
   --char_we
- char_we <= '1';
-  
  
-  
-  char_value <= 	-- "space" when char_address = X"0"
-						"000001"  when char_address = X"2" else 
-						"000010"  when char_address = X"3" else 
-						"000011"  when char_address = X"4" else 
-						"000100" when char_address = X"5" else 
-						"000000"; 
-						
+  char_we <= '1';
+
+ 
+  process(pix_clock_s)begin
+        if( rising_edge(pix_clock_s) )then
+            sec_cnt <= sec_cnt_next;
+            offset <= offset_next;
+           
+            if(char_address = "00010010110000") then
+                char_address <= "00000000000000";
+            else
+                char_address <= char_address + '1';
+            end if;
+        end if;
+  end process;
+ 
+  char_value <= "001011" when char_address ="00000001111000" + offset else
+                "000001" when char_address = "00000001111001" + offset else
+                "000011" when char_address = "00000001111010" + offset else
+                "000001" when char_address = "00000001111011" + offset else
+                "100000";
+                   
+    sec_cnt_next <= sec_cnt + 1 when sec_cnt < 01000000 else (others => '0');
+    offset_next <= offset when sec_cnt < 01000000 else
+                        offset + 1 when offset < 1100 else (others => '0');
+ 
   -- koristeci signale realizovati logiku koja pise po GRAPH_MEM
   --pixel_address
   --pixel_value
   --pixel_we
-  
-  
+ 
+    pixel_we<='1';
+    process(pix_clock_n) begin
+    if(rising_edge(pix_clock_n)) then
+        if(pixel_address="1001011000000000000") then
+            pixel_address<=(others=>'0');
+            counter_s<=counter_s+1;
+            if(counter_s = "00000100100") then 
+                add<=add+1;
+                if(add="0000010100") then
+                    add<=(others=>'0');
+                end if;
+                counter_s<=(others=>'0');
+            end if;
+        else
+            pixel_address<=pixel_address+1;
+        end if;
+    end if;
+    end process;
+   
+  pixel_value<=x"FFFFFFFF" when pixel_address=(x"00064"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00078"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"0008C"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"000A0"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"000B4"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"000C8"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"000DC"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"000F0"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00104"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00118"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"0012C"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00140"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00154"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00168"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"0017C"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00190"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"001A4"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"001B8"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"001CC"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"001E0"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"001F4"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00208"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"0021C"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00230"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00244"+add) else
+                    x"FFFFFFFF" when pixel_address=(x"00258"+add) else
+               
+                    x"00000000";
+
+                                                                               
+   
+ 
 end rtl;
